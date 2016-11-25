7b9a9445b5605bce99d2c109a71b8f49f9a1b887
Clay Graham <claytantor@gmail.com>
2016-11-25 05:17:14 +0000

    figuring out why the webhook isnt being created

--- application.py
+++ application.py
@@ -8,7 +8,6 @@ from flask import Flask, request, g, Response
 from werkzeug.local import LocalProxy
 
 app = Flask(__name__)
-# app.secret_key = os.environ.get("FLASK_SECRET_KEY", "soopersekrit")
 app.config.from_envvar('FLASK_APPLICATION_SETTINGS')
 
 def get_robot():
@@ -18,7 +17,27 @@ def get_robot():
         robot_instace = g._robot = robot.Robot(botdata)
     return robot_instace
 
+def init_smooch(flask_app):
+    jwt = smooch.get_jwt(flask_app.config["SMOOCH_KEY_ID"], flask_app.config['SMOOCH_SECRET'])
+    smooch_api_instance = smooch.SmoochV1(jwt=jwt)    
+
+    #test if there are any webhooks for this config
+    webhooks_available = smooch_api_instance.list_webhooks()
+
+    #check if the endpoint is in the config
+    found = False
+    for hook in webhooks_available['webhooks']:
+        if hook['target']==flask_app.config["SMOOCH_WEBOOK_ENDPOINT"]:
+            found=True
+
+    # tell smooch to start sending events to this place
+    if not found:
+        print "creating webhook:{0}".format(flask_app.config["SMOOCH_WEBOOK_ENDPOINT"])
+        create_response = smooch_api_instance.save_webhook(flask_app.config["SMOOCH_WEBOOK_ENDPOINT"], ["message:appUser", "postback"])
+	print create_response
+
 def get_smooch_api():
+    print "setting up smooch webhooks 2"
     smooch_api_instance = getattr(g, '_smooch_api', None)
     if smooch_api_instance is None:
 
@@ -37,7 +56,6 @@ def get_smooch_api():
         # tell smooch to start sending events to this place
         if not found:
             smooch_api_instance.save_webhook(app.config["SMOOCH_WEBOOK_ENDPOINT"], ["message:appUser", "postback"])
-
 
     return smooch_api_instance
 
@@ -78,10 +96,8 @@ def smooch_events():
     return resp
 
 def main(argv):
-    robot = LocalProxy(get_robot)
-    smooch_api = LocalProxy(get_smooch_api)
+    init_smooch(app)
     app.run(host='0.0.0.0', port=int(argv[0]), debug=True)
-    #app.run()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
